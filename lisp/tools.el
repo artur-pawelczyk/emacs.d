4dd1c814585df4d0ecf18116e66dae9100ff7b13
Artur Pawelczyk <pchairman@gmail.com>
2013-11-04 07:20:16 +0100

    Editing text from Vimperator.

--- lisp/tools.el
+++ lisp/tools.el
@@ -12,4 +12,17 @@
   (if (eq (require 'pymacs nil t) 'pymacs)
       (pymacs-load "ropemacs" "rope-")))
 
+(defun kill-temp-buffer-current ()
+  (interactive)
+  (save-buffer)
+  (kill-buffer)
+  (delete-frame))
+
+(defun input-form-find-file (filename)
+  (find-file filename)
+  (set-input-method "polish-slash")
+  (ispell-change-dictionary "polish")
+  (flyspell-mode)
+  (local-set-key (kbd "C-c C-c") 'kill-temp-buffer-current))
+
 (provide 'tools)
