9f32586608abb1b3507f68424ebd611246cbfff3
Artur Pawelczyk <pchairman@gmail.com>
2013-11-12 00:22:21 +0100

    Malabar-mode for Java.

--- conf/java.el
+++ conf/java.el
@@ -1,11 +1,13 @@
 (add-hook 'java-mode-hook (lambda ()
-			    (semantic-mode)
-			    (semantic-idle-completions-mode)
-			    (semantic-idle-summary-mode)
-			    (auto-complete-mode nil)
+ 			    (semantic-mode)
+                            (require 'malabar-mode)
+                            (add-to-list 'auto-mode-alist '("\\.java\\'" . malabar-mode))
+                            (setq malabar-groovy-lib-dir (user-file "lisp/malabar/lib"))
 			    (show-paren-mode)
-			    (define-key java-mode-map (kbd "M-/") 'semantic-ia-complete-symbol) 
+			    (define-key java-mode-map (kbd "M-/") 'semantic-complete-symbol) 
 			    (define-key java-mode-map (kbd "M-.") 'semantic-ia-fast-jump)
 			    ))
+
+(add-to-list 'load-path (user-file "lisp/malabar/lisp"))
 
 (provide 'conf/java)
