1731b374399310438dd8589d36230cf726bc099b
Artur Pawelczyk <pchairman@gmail.com>
2013-12-30 14:15:32 +0100

    Java configuration.

--- conf/java.el
+++ conf/java.el
@@ -3,11 +3,14 @@
                             (require 'malabar-mode)
                             (add-to-list 'auto-mode-alist '("\\.java\\'" . malabar-mode))
                             (setq malabar-groovy-lib-dir (user-file "lisp/malabar/lib"))
+                            (subword-mode)
 			    (show-paren-mode)
 			    (define-key java-mode-map (kbd "M-/") 'semantic-complete-symbol) 
 			    (define-key java-mode-map (kbd "M-.") 'semantic-ia-fast-jump)
+                            (c-set-style "gnu")
 			    ))
 
 (add-to-list 'load-path (user-file "lisp/malabar/lisp"))
+(add-to-list 'load-path (user-file "lisp/jdibug"))
 
 (provide 'conf/java)
