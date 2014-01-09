1aa30c8499e654524e3c828e4e4dce7122380f41
Artur Pawelczyk <pchairman@gmail.com>
2014-01-09 22:32:03 +0100

    Better packages.el.

--- lisp/conf/java.el
+++ lisp/conf/java.el
@@ -4,6 +4,7 @@
                             (add-to-list 'auto-mode-alist '("\\.java\\'" . malabar-mode))
                             (setq malabar-groovy-lib-dir (user-file "lisp/malabar/lib"))
                             (subword-mode)
+                            (electric-indent-mode)
 			    (show-paren-mode)
 			    (define-key java-mode-map (kbd "M-/") 'semantic-complete-symbol) 
 			    (define-key java-mode-map (kbd "M-.") 'semantic-ia-fast-jump)
