33b611f7aee7b1339d6fe52d4cc3a276fa6ce153
Artur Pawelczyk <pchairman@gmail.com>
2014-01-26 19:54:03 +0100

    Fixed dired configuration.

--- lisp/conf/dired.el
+++ lisp/conf/dired.el
@@ -1,13 +1,16 @@
-(setq dired-listing-switches "-hlDa")
-(setq dired-dwim-target t)
-
-(if (fboundp 'dired-hide-details-mode)
-    (add-hook 'dired-mode-hook 'dired-hide-details-mode))
-(define-key dired-mode-map (kbd "C-x M-h") 'dired-hide-details-mode)
-
 (add-hook 'dired-load-hook
 	  (lambda ()
 	    (load "dired-x")))
+
+(require 'dired)
+
+(setq dired-listing-switches "-hlDa")
+(setq dired-dwim-target t)
+
+(when (fboundp 'dired-hide-details-mode)
+    (add-hook 'dired-mode-hook 'dired-hide-details-mode)
+    (define-key dired-mode-map (kbd "C-x M-h") 'dired-hide-details-mode))
+
 (add-hook 'dired-mode-hook
 	  (lambda ()
 	    (setq dired-omit-files
