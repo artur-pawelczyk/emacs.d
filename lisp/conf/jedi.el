5a2e9bbe716aac69c5a10b39735a71bb1c0b8690
Artur Pawelczyk <pchairman@gmail.com>
2014-12-06 22:33:30 +0100

    Python3 for Jedi.

--- lisp/conf/jedi.el
+++
@@ -1,5 +0,0 @@
-(setq jedi:setup-keys t)
-(setq jedi:complete-on-dot t)
-(add-hook 'python-mode-hook 'jedi:setup)
-
-(provide 'conf/jedi)
--- lisp/conf/python.el
+++ lisp/conf/python.el
@@ -1,5 +1,11 @@
-(require 'conf/jedi)
+(require 'jedi)
 
+(setq jedi:setup-keys t)
+(setq jedi:complete-on-dot t)
+(setq jedi:environment-virtualenv
+      (append python-environment-virtualenv '("--python" "python3")))
+
+(add-hook 'python-mode-hook 'jedi:setup)
 (add-hook 'python-mode-hook (lambda ()
 			      (setq show-trailing-whitespace t)
 			      (set-fill-column 80)
