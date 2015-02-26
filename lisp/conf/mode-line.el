f1babd70db144ef632809dded6927252050cfb86
Artur Pawelczyk <pchairman@gmail.com>
2015-02-26 15:28:44 +0100

    Clean the mode line up from some minor mode information.

--- lisp/conf/mode-line.el
+++ lisp/conf/mode-line.el
@@ -3,4 +3,11 @@
 (setq sml/theme 'respectful)
 (eval-after-load 'init-finish '(sml/setup))
 
+(setq rm-blacklist '(" MRev" " Helm" " AC" " Undo-Tree" " Abbrev"))
+
+(defun projectile-disable-mode-line ()
+  (setq projectile-mode-line nil))
+
+(eval-after-load "projectile" #'projectile-disable-mode-line)
+
 (provide 'conf/mode-line)
