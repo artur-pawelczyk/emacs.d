72b056f5719048d94f8fb7fd4ee861166e4d5227
Artur Pawelczyk <pchairman@gmail.com>
2015-02-26 18:19:03 +0100

    Don't fail if Jedi is not present.

--- lisp/conf/python.el
+++ lisp/conf/python.el
@@ -5,8 +5,10 @@
 (setq jedi:environment-virtualenv
       (append python-environment-virtualenv '("--python" "python3")))
 
-(add-hook 'python-mode-hook #'jedi:setup)
-(add-hook 'python-mode-hook #'jedi:ac-setup)
+(when (require 'jedi nil 'noerror)
+  (add-hook 'python-mode-hook #'jedi:setup)
+  (add-hook 'python-mode-hook #'jedi:ac-setup))
+
 (add-hook 'python-mode-hook (lambda ()
 			      (setq show-trailing-whitespace t)
 			      (set-fill-column 80)))
