06c2e2f6d38d0223d613a97c2299c4e9e15ea8fb
Artur Pawelczyk <pchairman@gmail.com>
2015-02-26 15:27:39 +0100

    Auto-complete with Jedi.

--- lisp/conf/python.el
+++ lisp/conf/python.el
@@ -5,7 +5,8 @@
 (setq jedi:environment-virtualenv
       (append python-environment-virtualenv '("--python" "python3")))
 
-(add-hook 'python-mode-hook 'jedi:setup)
+(add-hook 'python-mode-hook #'jedi:setup)
+(add-hook 'python-mode-hook #'jedi:ac-setup)
 (add-hook 'python-mode-hook (lambda ()
 			      (setq show-trailing-whitespace t)
 			      (set-fill-column 80)
