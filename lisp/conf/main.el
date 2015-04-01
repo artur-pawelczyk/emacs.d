274bae85370ad85769fb4720e6c8b8e191e0cebd
Artur <artur.pawelczyk@yourvision.pl>
2015-04-01 13:28:28 +0200

    Call 'cygpath' on paths from compilation buffer.

--- lisp/conf/main.el
+++ lisp/conf/main.el
@@ -60,5 +60,13 @@
   (global-set-key (kbd "M-o") #'ace-window)
   (setq aw-scope 'frame))
 
+;; Translate dos path in compilation-mode
+(defun compilation-find-file--dos-path (orig-fun &rest args)
+  (let* ((path (cadr args))
+         (new-path (translate-dos-path path))
+         (new-args (cons (car args) (cons new-path (cddr args)))))
+    (apply orig-fun new-args)))
+
+(advice-add #'compilation-find-file :around #'compilation-find-file--dos-path)
 
 (provide 'conf/main)
