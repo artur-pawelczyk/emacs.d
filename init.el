beeb5e5eb5aa5a5def1c7a7961e85926118e3e11
Artur Pawelczyk <pchairman@gmail.com>
2015-04-30 02:18:31 +0200

    Autoload 'ji-projectile-build-database'.

--- init.el
+++ init.el
@@ -14,6 +14,7 @@
 (require 'git-timemachine-fix)
 (autoload 'ji-add "java-import-db" nil :interactive)
 (autoload 'ji-build-database "java-import-db" nil :interactive)
+(autoload 'ji-projectile-build-database "java-import-db" nil :interactive)
 (autoload 'ext-edit-region "ext-edit" nil :interactive)
 
 (require 'conf/main)
