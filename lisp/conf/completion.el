efebdcace3d077091f5329881c4eccb53ee2a8ef
Artur Pawelczyk <pchairman@gmail.com>
2015-06-16 01:12:06 +0200

    Helm adaptive mode.

--- lisp/conf/completion.el
+++ lisp/conf/completion.el
@@ -3,6 +3,11 @@
   (ido-mode 1)
   (ido-everywhere 1))
 
+(defun conf/enable-helm ()
+  (require 'helm)
+  (helm-mode t)
+  (helm-adaptive-mode t))
+
 (defun conf/helm-setup-keys ()
   (require 'helm-config)
   (global-set-key (kbd "C-x b") 'helm-buffers-list)
@@ -12,7 +17,7 @@
 (eval-after-load "helm" #'conf/helm-setup-keys)
 
 (if (require 'helm nil 'noerror)
-    (helm-mode t)
+    (conf/enable-helm)
   (conf/enable-ido))
 
 (defvar conf/imenu-function (if (require 'helm nil 'noerror)
