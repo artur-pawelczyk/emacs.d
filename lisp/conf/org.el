488174267a5cc72297f21b21c23012ae8afc7d53
Artur Pawelczyk <pchairman@gmail.com>
2015-06-30 23:55:03 +0200

    Org-crypt.

--- lisp/conf/org.el
+++ lisp/conf/org.el
@@ -1,3 +1,5 @@
+(setq org-modules '(org-bbdb org-bibtex org-crypt org-docview org-gnus org-habit org-info org-irc org-mhe org-rmail org-w3m))
+
 (with-package-lazy (org)
   (define-key org-mode-map (kbd "M-n") #'org-metadown)
   (define-key org-mode-map (kbd "M-p") #'org-metaup))
@@ -10,5 +12,12 @@
 (add-hook 'ediff-prepare-buffer-hook (lambda ()
                                        (when (eq major-mode 'org-mode)
                                          (visible-mode 1))))
+
+
+
+;; org-crypt
+(with-package-lazy (org org-crypt)
+  (org-crypt-use-before-save-magic)
+  (add-to-list 'org-tags-exclude-from-inheritance "crypt"))
 
 (provide 'conf/org)
