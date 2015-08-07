e01252b437fac8ffe86eb7483d3d244c66115b62
Artur Pawelczyk <artur.pawelczyk@yourvision.pl>
2015-08-07 17:58:55 +0200

    Prevent web-mode from inserting characters.
    Conflicts with smartpares.

--- lisp/conf/web.el
+++ lisp/conf/web.el
@@ -1,6 +1,9 @@
 (add-to-list 'auto-mode-alist '("\\.jsp\\'" . web-mode))
 
 (setq web-mode-enable-auto-closing nil)
+(setq web-mode-enable-auto-opening nil)
+(setq web-mode-enable-auto-pairing nil)
+(setq web-mode-enable-auto-quoting nil)
 
 (add-hook 'web-mode-hook #'subword-mode)
 
