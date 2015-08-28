ee8c6abcb2f98c2bd037d3c1aafcc78ec602c471
Artur Pawelczyk <artur.pawelczyk@yourvision.pl>
2015-08-28 17:18:42 +0200

    Helm-projectile hook.

--- lisp/conf/projects.el
+++ lisp/conf/projects.el
@@ -1,10 +1,20 @@
 (setq projectile-indexing-method 'alien)
+(setq projectile-switch-project-action #'projectile-dired)
 
 (with-package (projectile)
-  (projectile-global-mode)
-  (setq projectile-switch-project-action #'projectile-dired))
+  (projectile-global-mode))
 
 (with-package (helm-projectile)
   (helm-projectile-on))
+
+(defvar helm-projectile-hook nil)
+
+(defun helm-projectile--after-toggle (toggle &rest other)
+  (run-hooks 'helm-projectile-hook))
+
+(advice-add 'helm-projectile-toggle :after #'helm-projectile--after-toggle)
+
+(add-hook 'helm-projectile-hook (lambda ()
+                                  (setq projectile-switch-project-action #'projectile-dired)))
 
 (provide 'conf/projects)
