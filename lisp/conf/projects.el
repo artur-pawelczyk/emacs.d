f2d4b1c3fc2e63981b7c2ff8b4813e398d8a4526
Artur Pawelczyk <artur.pawelczyk@yourvision.pl>
2015-09-01 18:01:34 +0200

    Keys for projectile.
    Key bindings from helm-projectile in the regular projectile.

--- lisp/conf/projects.el
+++ lisp/conf/projects.el
@@ -1,8 +1,14 @@
 (setq projectile-indexing-method 'alien)
 (setq projectile-switch-project-action #'projectile-dired)
 
+(defun conf/projectile-helm-functionality ()
+  "Use some functions for `helm-projectile' even if it is not enabled."
+  (define-key projectile-command-map (kbd "s s") #'helm-projectile-ag))
+
 (with-package (projectile)
-  (projectile-global-mode))
+  (projectile-global-mode)
+  (when (package-installed-p 'helm-projectile)
+    (conf/projectile-helm-functionality)))
 
 (with-package (helm-projectile)
   (helm-projectile-on))
@@ -16,5 +22,7 @@
 
 (add-hook 'helm-projectile-hook (lambda ()
                                   (setq projectile-switch-project-action #'projectile-dired)))
+
+(add-hook 'helm-projectile-hook #'conf/projectile-helm-functionality)
 
 (provide 'conf/projects)
