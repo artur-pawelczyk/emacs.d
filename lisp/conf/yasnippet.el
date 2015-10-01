4e1e4b2a563484f78625e05306b7338ca8841ca2
Artur Pawelczyk <pchairman@gmail.com>
2015-10-01 21:42:06 +0200

    Yasnippet tweaks.

--- lisp/conf/yasnippet.el
+++ lisp/conf/yasnippet.el
@@ -1,1 +1,8 @@
 (provide 'conf/yasnippet)
+
+(setq yas-prompt-functions  '(yas-completing-prompt))
+
+;; Use only local snippets.  The variable is set always while loading the
+;; package.  Must be overwritten after loading.
+(with-package-lazy (yasnippet)
+  (setq yas-snippet-dirs (user-file "snippets")))
