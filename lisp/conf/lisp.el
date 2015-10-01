95c7117ffbf4755debebb28914ecec44a9243c56
Artur Pawelczyk <pchairman@gmail.com>
2015-10-01 23:58:43 +0200

    Eldoc for emacs-lisp-mode.

--- lisp/conf/lisp.el
+++ lisp/conf/lisp.el
@@ -1,4 +1,6 @@
 (add-hook 'emacs-lisp-mode-hook (lambda ()
                                   (local-set-key (kbd "M-.") #'find-function-at-point)))
 
+(add-hook 'emacs-lisp-mode-hook #'eldoc-mode)
+
 (provide 'conf/lisp)
