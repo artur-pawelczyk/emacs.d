4788c771260a4f6b15f913da6c8e5198cd9b67f5
Artur Pawelczyk <pchairman@gmail.com>
2016-01-20 00:44:58 +0100

    Enable manually installed Magit.

--- site-lisp/autoloads.el
+++ site-lisp/autoloads.el
@@ -1,2 +1,10 @@
 (autoload 'scilab-mode "scilab" nil :interactive)
 (autoload 'scilab-shell "scilab" nil :interactive)
+
+(add-to-list 'load-path (user-file "site-lisp/magit/lisp"))
+(when (file-exists-p (user-file "site-lisp/magit/lisp/magit-autoloads.el"))
+  (load (user-file "site-lisp/magit/lisp/magit-autoloads.el"))
+  (add-to-list 'conf/installed-packages 'magit))
+
+(with-package-lazy (info)
+  (add-to-list 'Info-directory-list (user-file "site-lisp/magit/Documentation/")))
