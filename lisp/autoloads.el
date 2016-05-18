d9c9cf4d8753ba8eb6a13fc3e9abfce799ee9f5e
Artur Pawelczyk <pchairman@gmail.com>
2016-05-18 13:04:35 +0200

    Hydra for org block movement.

--- lisp/autoloads.el
+++ lisp/autoloads.el
@@ -15,6 +15,8 @@
 (autoload 'hydras-magit/body "hydras" nil :interactive)
 (autoload 'hydras-forward-page "hydras" nil :interactive)
 (autoload 'hydras-backward-page "hydras" nil :interactive)
+(autoload 'hydras-org-next-block "hydras" nil :interactive)
+(autoload 'hydras-org-previous-block "hydras" nil :interactive)
 (autoload 'hydra-resize-window/body "hydra-resize-window" nil :interactive)
 
 (autoload 'umessage "umessage")
