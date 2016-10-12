e5a78e3b71a64e0972c473c57eec84d5b7776214
Artur Pawelczyk <pchairman@gmail.com>
2016-10-13 00:01:50 +0200

    umessage -- unit testing

--- lisp/umessage-test.el
+++ lisp/umessage-test.el
@@ -1,3 +1,5 @@
+(require 'dash)
+
 (ert-deftest umessage-format-error ()
   (should (equal (umessage-format-error '(quit) "")
                  "Quit"))
@@ -7,3 +9,38 @@
                  "Pre: Quit"))
   (should (equal (umessage-format-error '(text-read-only) "Pre" 'the-function)
                  "Pre: the-function: Text is read-only")))
+
+(ert-deftest umessage-window-size ()
+  (cl-labels ((test-window-height (text)
+                                  (with-temp-buffer
+                                    (insert text)
+                                    (window-total-height (umessage--new-window (current-buffer))))))
+    (should (equal (test-window-height "a") 1))
+    (should (equal (test-window-height "") 1))
+    (should (equal (test-window-height "a\n\a") 2))
+    (let ((umessage-window-max-height 3))
+      (should (equal (test-window-height "a\na\na\na\na") 3)))))
+
+(ert-deftest umessage-window-properties ()
+  (save-window-excursion
+    (let* ((buf (umessage--new-buffer (generate-new-buffer-name " *temp*")
+                                      "message"))
+           (win (umessage--new-window buf)))
+      (unwind-protect
+          (progn
+            (select-window win)
+            (should (null mode-line-format))
+            (should (null cursor-type))
+            (should (equal (point) 1))
+            (should (window-parameter (selected-window) 'no-other-window))
+            (should (window-dedicated-p)))
+        (kill-buffer buf)))))
+
+(ert-deftest umessage-log-only ()
+  (cl-letf* ((buf (generate-new-buffer " *temp*"))
+             ((symbol-function 'messages-buffer) (-const buf)))
+    (unwind-protect
+        (progn
+          (umessage-log-only "message")
+          (should (string-match-p "message" (with-current-buffer buf (buffer-string)))))
+      (kill-buffer buf))))
