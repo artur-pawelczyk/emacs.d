6b1858beb89d9b6d6451291a297c41b2177b0f92
Artur Pawelczyk <pchairman@gmail.com>
2016-12-20 00:05:04 +0100

    Prevent Semantic from scanning all buffers before closing Emacs.

--- lisp/conf/cc.el
+++ lisp/conf/cc.el
@@ -1,6 +1,6 @@
 ;; C-like languages (based on `cc-mode').
 
-(setq semantic-default-submodes '(global-semanticdb-minor-mode))
+(setq semantic-default-submodes nil)
 
 (with-package-lazy (cc-mode)
   ;; Semantic is a global mode.
