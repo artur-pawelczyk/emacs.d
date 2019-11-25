8a45c4f9b50f24c2ea37067943bdbd3a6ed03a71
Artur Pawelczyk <apawelczyk@atlassian.com>
2019-11-25 19:03:17 +0100

    Support for zoom-mode

--- lisp/conf/windows.el
+++ lisp/conf/windows.el
@@ -1,3 +1,8 @@
 (with-package (golden-ratio)
   (add-hook 'golden-ratio-mode-hook
             (lambda () (advice-add 'ace-window :after #'golden-ratio))))
+
+(setq zoom-size '(0.618 . 0.618))
+
+(with-package (zoom)
+  (zoom-mode))
