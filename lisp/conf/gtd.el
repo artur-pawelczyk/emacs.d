67baf62437bdfd6839ad77808978b4994ea7a028
Artur Pawelczyk <apawelczyk@atlassian.com>
2020-02-18 14:28:17 +0100

    GTD config

---
+++ lisp/conf/gtd.el
@@ -0,0 +1,13 @@
+(setq org-babel-tangle-use-relative-file-links nil)
+
+(with-package (org-capture)
+  (add-to-list 'org-capture-templates `("t" "Todo" entry
+                                        (file ,(format "~/org/inbox-%s.org" system-name))
+                                        "* TODO %?\n%U"))
+  (add-to-list 'org-capture-templates '("T" "Old-style todo" entry
+                                        (file+headline "~/org/notes.org" "Inbox")
+                                        "* TODO %?\n%U"))
+  (add-to-list 'org-capture-templates '("e" "Event" entry
+                                        (file+headline "~/org/notes.org" "Inbox")))
+  (add-to-list 'org-capture-templates '("z" "Shopping list item" item
+                                        (file+headline "~/org/notes.org" "Shopping list"))))
