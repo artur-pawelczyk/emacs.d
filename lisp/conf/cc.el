6848d380c3deaa42a4dd6cc3d4a569ae59f91293
Artur Pawelczyk <apawelczyk@atlassian.com>
2020-11-18 17:21:55 +0100

    Disable semantic by default

--- lisp/conf/cc.el
+++ lisp/conf/cc.el
@@ -2,9 +2,5 @@
 
 (setq semantic-default-submodes nil)
 
-(with-package-lazy (cc-mode)
-  ;; Semantic is a global mode.
-  (semantic-mode 1))
-
 (when (conf/installed-p 'ggtags)
   (add-hook 'c-mode-hook #'ggtags-mode))
