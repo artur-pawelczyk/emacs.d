ea4b59611590c81d2c49f983b44c173b1c8f1112
Artur Pawelczyk <pchairman@gmail.com>
2023-04-26 00:43:08 +0200

    god-mode configuration

--- lisp/conf/keys.el
+++ lisp/conf/keys.el
@@ -1,7 +1,10 @@
+(setq god-mode-enable-function-key-translation nil)
+
 (with-package (god-mode)
   (god-mode)
   (global-set-key (kbd "<escape>") #'god-local-mode)
   (define-key god-local-mode-map (kbd ".") #'repeat)
+  (define-key god-local-mode-map (kbd "i") #'god-local-mode)
   (global-set-key (kbd "C-x C-1") #'delete-other-windows)
   (global-set-key (kbd "C-x C-2") #'split-window-below)
   (global-set-key (kbd "C-x C-3") #'split-window-right)
