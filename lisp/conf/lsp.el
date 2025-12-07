9602c069f3e03d5ce1ce09e0d2bcd24531cc8e55
Artur Pawelczyk <pchairman@gmail.com>
2025-12-07 16:53:52 +0100

    flycheck keybinding for god-mode

--- lisp/conf/lsp.el
+++ lisp/conf/lsp.el
@@ -1,3 +1,6 @@
 (with-package-lazy (lsp-mode)
   (define-key lsp-mode-map (kbd "C-c M-.") #'lsp-find-implementation)
   (define-key lsp-mode-map (kbd "C-c C-c") lsp-command-map))
+
+(with-package-lazy (flycheck)
+  (define-key flycheck-mode-map (kbd "C-c C-1") flycheck-command-map))
