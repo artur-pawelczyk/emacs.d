cf6a110d023e0659f306793768027dc9900a9941
Artur Pawelczyk <pchairman@gmail.com>
2026-01-23 22:16:03 +0100

    Configure elisp mode

--- lisp/conf/lisp.el
+++ lisp/conf/lisp.el
@@ -3,3 +3,13 @@
 
 (when (fboundp 'global-eldoc-mode)
   (global-eldoc-mode 1))
+
+(use-package company
+  :commands company-mode
+  :defer t
+  :hook (emacs-lisp-mode . company-mode))
+
+(use-package smartparens
+  :commands smartparens-mode
+  :defer t
+  :hook (emacs-lisp-mode . smartparens-mode))
