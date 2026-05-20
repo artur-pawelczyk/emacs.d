687dd4b432bfe05d7576b5476a0293f0899e5ff5
Artur Pawelczyk <pchairman@gmail.com>
2026-05-20 16:56:14 +0200

    Move smartparens configuration to use-package

--- lisp/conf/lisp.el
+++ lisp/conf/lisp.el
@@ -12,4 +12,18 @@
 (use-package smartparens
   :commands smartparens-mode
   :defer t
-  :hook (emacs-lisp-mode . smartparens-mode))
+  :init (require 'smartparens-config nil :noerror)
+  :hook (emacs-lisp-mode . smartparens-mode)
+  :bind (:map smartparens-mode-map
+              ("C-M-f" . sp-forward-sexp)
+              ("C-M-b" . sp-backward-sexp)
+              ("C-M-u" . sp-backward-up-sexp)
+              ("C-M-d" . sp-down-sexp)
+              ("C-M-p" . sp-backward-down-sexp)
+              ("C-M-n" . sp-up-sexp)
+              ("C-M-k" . sp-kill-sexp)
+              ("C-M-t" . sp-transpose-sexp)
+              ("C-)" . sp-forward-slurp-sexp)
+              ("C-}" . sp-forward-barf-sexp)
+              ("C-(" . sp-backward-slurp-sexp)
+              ("C-{" . sp-backward-barf-sexp)))
